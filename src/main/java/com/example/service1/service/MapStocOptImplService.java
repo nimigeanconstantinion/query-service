8cd90fc030641f3f37a81a80b6951dc3b7a39915
nimigeanconstantinion <constantin.nimigean@gmail.com>
2024-12-16 18:44:02 +0200

    cu appl..-kubernetes

--- src/main/java/com/example/service1/service/MapStocOptImplService.java
+++ src/main/java/com/example/service1/service/MapStocOptImplService.java
@@ -2,12 +2,14 @@ package com.example.service1.service;
 
 import com.example.service1.model.MapStocOptim;
 import com.example.service1.repository.StocOptimRepo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class MapStocOptImplService implements MapStocOptService{
 
     private StocOptimRepo stocOptimRepo;
@@ -27,6 +29,7 @@ public class MapStocOptImplService implements MapStocOptService{
     public MapStocOptim getMapStocOptByID(String idA) {
         Optional<MapStocOptim> opm= stocOptimRepo.findByIdArticol(idA);
         if(!opm.isPresent()){
+            log.error("querySrvFailID: "+idA);
             throw new RuntimeException("ID-ul nu exista!!");
         }
         return opm.get();
